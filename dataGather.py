2c4bcc54b36ba5289665ab3d685ad6944445d591
Assaf <Assaf.Yossifoff@kcl.ac.uk>
2015-02-28 17:05:49 +0000

    data should be printed when running dataGather. Can change print to json.load to keep the information

--- dataGather.py
+++ dataGather.py
@@ -1,5 +1,42 @@
-#Assif is taking point.
-def get(target,currency,amount,time):
-	
-    pass
-    
+#!/usr/bin/env python2
+
+# usage: python HistoricalDataRequest.py <host-ip>
+
+import argparse
+import json
+import ssl
+import sys
+import urllib2
+
+data = {
+        "securities": ["XBT Curncy"],
+        "fields": ["PX_LAST"],
+        "startDate": "20120101",
+        "endDate": "20150101",
+        "periodicitySelection": "DAILY"
+}
+
+
+def request():
+    req = urllib2.Request('https://http-api.openbloomberg.com/request?ns=blp&service=refdata&type=HistoricalDataRequest')
+    req.add_header('Content-Type', 'application/json')
+
+    ctx = ssl.SSLContext(ssl.PROTOCOL_SSLv23)
+    ctx.load_verify_locations('bloomberg.crt')
+    ctx.load_cert_chain('client.crt', 'client.key')
+
+    try:
+        res = urllib2.urlopen(req, data=json.dumps(data), context=ctx)
+        print res.read()
+    except Exception as e:
+        e
+        print e
+        return 1
+    return 0
+
+
+def main():
+    return request()
+
+if __name__ == "__main__":
+    sys.exit(main())
